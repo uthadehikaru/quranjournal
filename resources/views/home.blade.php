d39ef9a0810bf071145f5f1a8d26e5aa6925caa1
zuhriutama <zuhriutama@gmail.com>
2024-08-02 08:51:10 +0700

    remove product and artikel on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -79,43 +79,4 @@
         @endforeach
     </div>
 </section>
-<section class="text-gray-600 body-font py-8">
-    <div class="container mx-auto flex py-4 flex-row justify-between">
-        <a href="{{ route('pages.show','profile') }}"><h2 class="text-xl text-primary-default px-2 font-bold">Tentang Qur'an Journal</h2></a>
-        <a href="https://instagram.com/thequranjournal.id" class="text-sm bg-secondary-lighter text-primary-default p-2 mx-2 border border-secondary-default rounded">
-          Follow Us
-        </a>
-    </div>
-    @if($video_homepage)
-    <div class="container mx-auto flex flex-row justify-around items-center">
-    {!! $video_homepage->value !!}
-    </div>
-    @endif
-</section>
-<section class="text-gray-600 body-font">
-    <div class="container mx-auto flex py-2 flex-row justify-between">
-        <h2 class="text-xl text-primary-default p-2 font-bold">Buku dan Printable</h2>
-        <a href="{{ route('produk.index') }}" class="text-sm bg-secondary-lighter text-primary-default p-2 mx-2 border border-secondary-default rounded">
-          Lihat semua
-        </a>
-    </div>
-    <div class="container mx-auto grid grid-cols-3 gap-4 px-2 justify-around items-center">
-        @foreach ($products as $product)
-        <x-post-card :post="$product" route="products" />
-        @endforeach
-    </div>
-</section>
-<section class="text-gray-600 body-font">
-    <div class="container mx-auto flex py-2 flex-row justify-between">
-        <h2 class="text-xl text-primary-default p-2 font-bold">Artikel</h2>
-        <a href="{{ route('artikel.index') }}" class="text-sm bg-secondary-lighter text-primary-default p-2 mx-2 border border-secondary-default rounded">
-          Lihat semua
-        </a>
-    </div>
-    <div class="container mx-auto grid grid-cols-3 gap-4 px-2 justify-around items-center">
-        @foreach ($articles as $article)
-        <x-post-card :post="$article" route="posts" />
-        @endforeach
-    </div>
-</section>
 @endsection
